be29c41a9544a77aa7ca6a1aa659c7ca3f1c963b
rain-soaked-eli <ewiwiin@gmail.com>
2026-02-14 00:54:54 +0800

    Add files via upload

---
+++ database.js
@@ -0,0 +1,37 @@
+/* 
+{
+        date: "2026-02-14",
+        title: "Entry XX: TITLE HERE",
+        content: `
+            <p>TYPE YOUR TEXT HERE.</p>
+            <p>USE ANOTHER P TAG FOR A NEW PARAGRAPH.</p>
+        `
+    },
+*/
+
+const blogPosts = [
+{
+        date: "2026-02-14",
+        title: "Entry 03:",
+        content: `
+            <p>I think im getting used to this shxt.</p>
+            <p>I'll be fine</p>
+        `
+    },
+    {
+        date: "2026-02-14",
+        title: "Entry 02: System Check",
+        content: `
+            <p>Okay, I think I have the database working now.</p>
+            <p>I can just type my thoughts here and they appear on the site. It feels like sending a message into the void.</p>
+        `
+    },
+    {
+        date: "2026-02-14",
+        title: "Entry 01: Hello World",
+        content: `
+            <p>Initiating sequence...</p>
+            <p>Everything is connected.</p>
+        `
+    }
+];
