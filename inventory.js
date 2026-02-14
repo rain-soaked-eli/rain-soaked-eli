69b22fa99c490f0503825355c09aa074c4ebd70f
rain-soaked-eli <ewiwiin@gmail.com>
2026-02-14 19:05:42 +0800

    Add files via upload

---
+++ inventory.js
@@ -0,0 +1,18 @@
+/* ADD NEW POSTS HERE. 
+   The script will build the "Entry Cards" for you.
+*/
+const inventory = [
+    // NEWEST POSTS GO TOP
+    {
+        file: "rant_02.html",
+        title: "Why I Hate Physics",
+        date: "2026-02-15",
+        desc: "Kinematics makes no sense and here is why."
+    },
+    {
+        file: "rant_01.html",
+        title: "Hello World",
+        date: "2026-02-14",
+        desc: "My first entry in the deep archives."
+    }
+]
