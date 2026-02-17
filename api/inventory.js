a345f3b8546d16558f1ac3308de4f115e89e899d
rain-soaked-eli <ewiwiin@gmail.com>
2026-02-17 13:59:49 +0800

    Add files via upload

---
+++ api/inventory.js
@@ -0,0 +1,21 @@
+/* ADD NEW POSTS HERE. 
+   The script will build the "Entry Cards" for you.
+*/
+const inventory = [
+    // NEWEST POSTS GO TOP
+    {
+        file: "rant_02.html",
+        title: "The Beauty of Unraveling",
+        date: "2026-02-14",
+        desc: "Reflects on decay, impermanence, and why deterioration feels more honest than anything polished."
+    }
+   
+    {
+        file: "rant_01.html",
+        title: "Anti-Performative Identity",
+        date: "2026-02-14",
+        desc: "Most people are acting. Few are conscious of it."
+    }
+
+]
+
